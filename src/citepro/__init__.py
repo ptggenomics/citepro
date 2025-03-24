1e96f0ce8086f2abda30a537e43bf2aee0d69f55
jasonhuang <jasonhuang@ptglab.com>
2025-03-24 11:11:55 -0700

    try1 for cudf zero change

--- src/citepro/__init__.py
+++ src/citepro/__init__.py
@@ -1,4 +1,17 @@
 import sys
+import logging
+logger = logging.getLogger('citepro')
+logger.setLevel(logging.INFO)
+
+try:
+    import cudf
+    use_gpu = True
+    logger.info("found cudf, try using cudf zero code change mode")
+    import cudf.pandas
+    cudf.pandas.install()
+except ModuleNotFoundError:
+    use_gpu = False
+    logger.info("rapids_singlecell not installed, fallback to CPU")
 
 from . import recipe
 from . import io
